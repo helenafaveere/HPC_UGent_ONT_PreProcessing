fd289de105e2af37d8e44bae315ac84f0660faa9
vsc45900 vscuser <vsc45900@gligar08.gastly.os>
2024-05-30 12:54:24 +0200

    v3 successful GPU parallelisation

--- ONT_PrePro_Pipeline.sh
+++ ONT_PrePro_Pipeline.sh
@@ -1,40 +1,34 @@
-#!/bin/bash -l
+#!/bin/bash -l 
 
 #SBATCH --time=72:00:00
-#SBATCH --mem=250gb
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=16   
-
-#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-task=2
+#SBATCH --mem-per-gpu=32G
+#SBATCH --ntasks=2
 #SBATCH --mail-user=justine.rayp@ugent.be
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=ONT-PreProc-Pipeline-J
+#SBATCH --job-name=Main_Script_Test6-J
 
 ###------------------------------------------------- module loading
 module purge
-module unload GCC/13.2.0
-module unload GCCcore/13.2.0
-module unload zlib/1.2.13-GCCcore-13.2.0
-module unload binutils/2.40-GCCcore-13.2.0
-module load GCCcore/11.2.0
-module load GCC/11.2.0
-module load SAMtools/1.14-GCC-11.2.0
-module load minimap2/2.22-GCCcore-11.2.0
+#module unload GCC/13.2.0
+#module unload GCCcore/13.2.0
+#module unload zlib/1.2.13-GCCcore-13.2.0
+#module unload binutils/2.40-GCCcore-13.2.0
+module load GCCcore/11.3.0
+module load GCC/11.3.0
+module load SAMtools/1.16.1-GCC-11.3.0
+module load pod5-file-format/0.1.8-foss-2022a
+module load minimap2/2.24-GCCcore-11.3.0
 export PATH=/scratch/gent/vo/001/gvo00115/vsc45900/dorado-0.4.0-linux-x64/bin:${PATH} # Add path to dorado installation
 export PATH=/scratch/gent/vo/001/gvo00115/vsc45900/miniforge3/envs/pycoqc_252/bin:${PATH} # Set this to the right path to your installation of pycoqc
 export PATH=/scratch/gent/vo/001/gvo00115/vsc45900/miniforge3/envs/modkit/bin:${PATH} # Add the path to your modkit installation
-conda activate wisecodorXv1.2.5 #activate your wisecondorx environment
+CONDA_ENV_NAME=wisecodorXv1.2.5 #the name of your wisecondorX environment
 
-###------------------------------------------------- function definition
-Fast5_2_Pod5() {
-   # Convert Pod5 to Fast5
-   pod5 convert fast5 "${1}"/*.fast5 output_pod5s --one-to-one ./
-}
 
 ###------------------------------------------------- flag definition
-DEFAULT_KIT_NAME="SQK-NBD114-24"
-DEFAULT_CONFIG="dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
-DEFAULT_WISECONDORREF="/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/WisecondorX_ref/LQB.GRCh38.100kb.npz"
+
 
 # Check for provided options
 while getopts "p:t:w:n:r:k:c:W:" flag; do
@@ -50,7 +44,11 @@ while getopts "p:t:w:n:r:k:c:W:" flag; do
     esac
 done
 
-# Set default values if not provided
+#default values
+DEFAULT_KIT_NAME="SQK-NBD114-24"
+DEFAULT_CONFIG="dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
+DEFAULT_WISECONDORREF="/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/WisecondorX_ref/LQB.GRCh38.100kb.npz"
+
 if [[ -z "$KIT_NAME" ]]; then
     KIT_NAME="$DEFAULT_KIT_NAME"
 fi
@@ -69,8 +67,9 @@ echo "WISECONDORREF: $WISECONDORREF"
 
 ###------------------------------------------------- converting to the right datatype
 if [ "$InputDataType" = "fast5" ]; then
-    Fast5_2_Pod5 "$InputDataPath"
-    READSDIR="/outputpod5s"
+    pod5 convert fast5 -r -O "$InputDataPath" "$InputDataPath"/*.fast5 "$InputDataPath"/output_pod5s
+    #Fast5_2_Pod5 "$InputDataPath"
+    READSDIR="$InputDataPath"/"output_pod5s"
 elif [ "$InputDataType" = "pod5" ]; then
     echo "Handling pod5 data type"
     READSDIR="$InputDataPath"
@@ -104,14 +103,14 @@ dorado download --model ${CONFIG}
 
 ###--------------------------------------------- simplex basecalling (background process)
 
-srun --cpu-bind=cores --cpus-per-task=16 bash -c '
+srun --ntasks=1 --exclusive bash -c '
     echo "start basecalling" &&
     echo ${CONFIG} &&
     echo ${KIT_NAME} &&
     echo ${WORKDIR}/${CONFIG} &&
     echo ${READSDIR} &&
     dorado basecaller \
-        --device "cuda:0,1,2,3" \
+        --device "cuda:0,1" \
         --batchsize ${batch_size} \
         --chunksize ${chunk_size} \
         --recursive \
@@ -128,9 +127,9 @@ srun --cpu-bind=cores --cpus-per-task=16 bash -c '
 
 ###--------------------------------------------- methylation analysis (background process)
 
-srun --cpu-bind=cores --cpus-per-task=16 bash -c '
+srun --ntasks=1 --exclusive bash -c '
     dorado basecaller \
-        --device "cuda:0,1,2,3" \
+        --device "cuda:0,1" \
         --batchsize "${batch_size}" \
         --chunksize "${chunk_size}" \
         --modified-bases 5mCG_5hmCG \
@@ -200,10 +199,12 @@ do
     sample_num=$(printf "%02d" "$i")
     
     samtools view "basecalling/${KIT_NAME}_barcode${sample_num}.bam" | awk '{print length($10)}' > "ReadLenghts/all_read_lengths_barcode${sample_num}.txt"
+    samtools view "basecalling/simplex_mapped_barcode${sample_num}.bam" | awk '{print length($10)}' > "ReadLenghts/all_mapped_read_lengths_barcode${sample_num}.txt"
 done
 
 
 ###----------------------------------------------- running WisecondorX to study CNVs (generates plots and BED files)
+ conda activate $CONDA_ENV_NAME #activate wisecondorx environment
 
 export WISECONDORREF=${WISECONDORREF}
 mkdir -p "${WORKDIR}/WisecondorX" 
@@ -211,9 +212,13 @@ mkdir -p "${WORKDIR}/WisecondorX"
 for ((i=1; i<=num_samples; i++))
 do
     sample_num=$(printf "%02d" "$i")
+    output_folder="barcode${i}"
+    mkdir -p "${WORKDIR}/WisecondorX/${output_folder}" 
     
     WisecondorX convert "basecalling/simplex_mapped_barcode${sample_num}.bam" "WisecondorX/simplex_mapped_barcode${sample_num}.npz"
     
     WisecondorX predict "WisecondorX/simplex_mapped_barcode${sample_num}.npz" ${WISECONDORREF} barcode${sample_num} --plot --bed
     
+    mv barcode${sample_num}* "${WORKDIR}/WisecondorX/${output_folder}/"
+
 done
