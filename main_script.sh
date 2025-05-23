bb92d417e8acc004b741015b07178231d4eeaf60
helenafaveere <helenafaveere@gmail.com>
2025-05-23 14:36:42 +0200

    Update and rename ONT_PrePro_Pipeline.sh to main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -1,36 +1,35 @@
 #!/bin/bash -l 
 
 #SBATCH --time=72:00:00
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=16
-#SBATCH --gpus-per-task=2
-#SBATCH --mem-per-gpu=32G
+#SBATCH --nodes=2
 #SBATCH --ntasks=2
+#SBATCH --cpus-per-task=24
+#SBATCH --gpus-per-task=4
+#SBATCH --mem-per-gpu=80G
+#SBATCH --output=main_script_%j.log 
+#SBATCH --error=main_script_%j.err 
 #SBATCH --mail-user=helena.faveere@ugent.be
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=Main_Script_TestRunOctober
+#SBATCH --job-name=main_script
 
 ###------------------------------------------------- module loading
 module purge
-#module unload GCC/13.2.0
-#module unload GCCcore/13.2.0
-#module unload zlib/1.2.13-GCCcore-13.2.0
-#module unload binutils/2.40-GCCcore-13.2.0
-module load GCCcore/11.3.0
-module load GCC/11.3.0
-module load SAMtools/1.16.1-GCC-11.3.0
-module load pod5-file-format/0.1.8-foss-2022a
-module load minimap2/2.24-GCCcore-11.3.0
-export PATH= /kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/ONT_env/bin:${PATH} 
-#Add path to dorado, pycoqc, modkit and wisecondorX environment, all in the same file!!
+module load GCCcore/12.3.0
+module load GCC/12.3.0
+module load SAMtools/1.18-GCC-12.3.0
+module load pod5-file-format/0.3.10-foss-2023a
+module load minimap2/2.26-GCCcore-12.3.0
+module load R/4.3.3-gfbf-2023b
 
-#export PATH=/scratch/gent/vo/001/gvo00115/vsc45900/dorado-0.4.0-linux-x64/bin:${PATH} # Add path to dorado installation
-#export PATH=/scratch/gent/vo/001/gvo00115/vsc45900/miniforge3/bin:${PATH} # Set this to the right path to your installation of pycoqc
-#export PATH=/scratch/gent/vo/001/gvo00115/vsc45900/miniforge3/envs/modkit/bin:${PATH} # Add the path to your modkit installation
-#CONDA_ENV_NAME=wisecodorXv1.2.5 #the name of your wisecondorX environment
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/dorado-0.8.2-linux-x64/bin:${PATH} # Add path to your Dorado installation
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/miniforge3/envs/pycoqc_env/bin:${PATH} # Add path to your pycoQC installation
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/miniforge3/envs/modkit_env/bin:${PATH} # Add path to your modkit installation
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/Rlibs/bin:${PATH} # Add path to your R environment
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/miniforge3/envs/wisecondorx_env/bin:${PATH} # Add path to your WisecondorX installation
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/miniforge3/envs/methylartist_env/bin:${PATH} # Add path to your Methylartist installation
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/miniforge3/envs/nanocaller_env/bin:${PATH} # Add path to your Nanocaller installation
+export PATH=/kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/miniforge3/envs/whatshap_env/bin:${PATH} # Add path to your Whatshap installation
 
-conda activate ./kyukon/data/gent/shared/001/gvo00115/ONT_cfDNA/Tools/ONT_env/ #activate wisecondorx environment
-# better than just given a name to wisecondorX environment
 ###------------------------------------------------- flag definition
 
 
